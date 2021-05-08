249f8718c3975300b62b3726dcc3d25555b2a897
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-08 09:43:04 -0400

    prior specified as functions

--- hq-config/prior.py
+++ hq-config/prior.py
@@ -5,33 +5,51 @@ import exoplanet.units as xu
 from exoplanet.distributions import Angle
 import thejoker as tj
 
-# The prior used to run The Joker: please edit below
-with pm.Model() as model:
-    s = xu.with_unit(pm.Lognormal('s', -3.5, 1),
-                     u.km/u.s)
 
-    prior = tj.JokerPrior.default(
-        P_min=1.5*u.day, P_max=16384*u.day,
-        sigma_K0=30*u.km/u.s,
-        sigma_v=100*u.km/u.s,
-        s=s
-    )
+defaults = {
+    'P_min': 1.5 * u.day,
+    'P_max': 16384 * u.day,
+    'sigma_v': 150 * u.km/u.s,
+    'sigma_K0': 30 * u.km/u.s
+}
 
-with pm.Model() as model:
-    s = xu.with_unit(pm.Lognormal('s', -3.5, 1),
-                     u.km/u.s)
 
-    # See note above: when running MCMC, we will sample in the parameters
-    # (M0 - omega, omega) instead of (M0, omega)
-    M0_m_omega = xu.with_unit(Angle('M0_m_omega'), u.radian)
-    omega = xu.with_unit(Angle('omega'), u.radian)
-    M0 = xu.with_unit(pm.Deterministic('M0', M0_m_omega + omega),
-                      u.radian)
+def get_prior(**kwargs):
+    for k, v in defaults.items():
+        kwargs.setdefault(k, v)
 
-    prior_mcmc = tj.JokerPrior.default(
-        P_min=1.5*u.day, P_max=16384*u.day,
-        sigma_K0=30*u.km/u.s,
-        sigma_v=150*u.km/u.s,
-        s=s,
-        pars={'M0': M0, 'omega': omega}
-    )
+    with pm.Model() as model:
+        s = xu.with_unit(pm.Lognormal('s', -3.5, 1),
+                         u.km/u.s)
+
+        prior = tj.JokerPrior.default(
+            P_min=kwargs['P_min'], P_max=kwargs['P_max'],
+            sigma_K0=kwargs['sigma_K0'],
+            sigma_v=kwargs['sigma_v'],
+            s=s
+        )
+
+    return prior, model
+
+
+def get_prior_mcmc(**kwargs):
+    with pm.Model() as model:
+        s = xu.with_unit(pm.Lognormal('s', -3.5, 1),
+                         u.km/u.s)
+
+        # When running MCMC, we will sample in the parameters
+        #     (M0 - omega, M0 + omega) instead of (M0, omega)
+        M0_m_omega = xu.with_unit(Angle('M0_m_omega'), u.radian)
+        omega = xu.with_unit(Angle('omega'), u.radian)
+        M0 = xu.with_unit(pm.Deterministic('M0', M0_m_omega + omega),
+                          u.radian)
+
+        prior_mcmc = tj.JokerPrior.default(
+            P_min=kwargs['P_min'], P_max=kwargs['P_max'],
+            sigma_K0=kwargs['sigma_K0'],
+            sigma_v=kwargs['sigma_v'],
+            s=s,
+            pars={'M0': M0, 'omega': omega}
+        )
+
+    return prior_mcmc, model
