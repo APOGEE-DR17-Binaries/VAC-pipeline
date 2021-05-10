6cf0f088c312e4083e5b67f000cabc23721e2673
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-10 14:57:52 -0400

    lnP not P

--- hq-config/prior.py
+++ hq-config/prior.py
@@ -3,7 +3,9 @@ import astropy.units as u
 import pymc3 as pm
 import exoplanet.units as xu
 from exoplanet.distributions import Angle
+import numpy as np
 import thejoker as tj
+import theano.tensor as tt
 
 
 defaults = {
@@ -36,15 +38,25 @@ def get_prior(**kwargs):
     return prior, model
 
 
-def get_prior_mcmc(**kwargs):
+def get_prior_mcmc(fixed_s=False, **kwargs):
     for k, v in defaults.items():
         kwargs.setdefault(k, v)
 
     with pm.Model() as model:
-        s = xu.with_unit(
-            pm.Lognormal('s', kwargs['logs_mean'], kwargs['logs_std']),
-            u.km/u.s
-        )
+        lnP = pm.Uniform('lnP',
+                         np.log(kwargs['P_min'].to_value(u.day)),
+                         np.log(kwargs['P_max'].to_value(u.day)))
+        P = xu.with_unit(
+            pm.Deterministic('P', tt.exp(lnP)),
+            u.day)
+
+        if fixed_s is False:
+            s = pm.Bound(pm.Lognormal, 0, 5.)('s',
+                                              kwargs['logs_mean'],
+                                              kwargs['logs_std'])
+            s = xu.with_unit(s, u.km/u.s)
+        else:
+            s = fixed_s
 
         # When running MCMC, we will sample in the parameters
         #     (M0 - omega, M0 + omega) instead of (M0, omega)
@@ -54,11 +66,10 @@ def get_prior_mcmc(**kwargs):
                           u.radian)
 
         prior_mcmc = tj.JokerPrior.default(
-            P_min=kwargs['P_min'], P_max=kwargs['P_max'],
             sigma_K0=kwargs['sigma_K0'],
             sigma_v=kwargs['sigma_v'],
             s=s,
-            pars={'M0': M0, 'omega': omega}
+            pars={'P': P, 'M0': M0, 'omega': omega}
         )
 
     return prior_mcmc, model
