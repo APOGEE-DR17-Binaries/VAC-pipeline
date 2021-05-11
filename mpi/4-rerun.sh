5b93c90b4d9457a9d64741607eaa4e479eecf16e
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-10 23:03:12 -0400

    rerun

---
+++ mpi/4-rerun.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J apogee-rerun
+#SBATCH -o logs/apogee-rerun.o%j
+#SBATCH -e logs/apogee-rerun.e%j
+#SBATCH -N 10
+#SBATCH --ntasks-per-node=64
+#SBATCH -t 36:00:00
+#SBATCH -p cca
+#SBATCH --constraint=rome
+
+source ~/.bash_profile
+init_conda
+conda activate dr17-binaries
+echo $HQ_RUN_PATH
+
+cd /mnt/ceph/users/apricewhelan/projects/apogee-dr17-binaries
+
+date
+
+mpirun python3 -m mpi4py.run -rc thread_level='funneled' \
+$CONDA_PREFIX/bin/hq rerun_thejoker -v --mpi
+
+date
+
