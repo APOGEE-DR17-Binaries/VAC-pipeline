20bee8fef45e037aef3070c64fc899fc258b8f4e
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-25 10:40:35 -0400

    tar up the samples

---
+++ mpi/9-b-tar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J apogee-tar
+#SBATCH -o logs/apogee-tar.o%j
+#SBATCH -e logs/apogee-tar.e%j
+#SBATCH -n 1
+#SBATCH -t 12:00:00
+#SBATCH -p cca
+
+source ~/.bash_profile
+
+cd /mnt/ceph/users/apricewhelan/projects/apogee-dr17-binaries/cache/hq
+
+date
+
+tar -czf samples.tar.gz samples
+
+date
+
